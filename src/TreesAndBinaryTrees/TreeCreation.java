feacb0d3ea27d4fe02e476e9d44abcdb34f36a99
attrighosal <attrighosal@gmail.com>
2022-11-29 21:05:07 +0530

    Added Code for tree creation

--- src/TreesAndBinaryTrees/TreeCreation.java
+++ src/TreesAndBinaryTrees/TreeCreation.java
@@ -1,4 +1,22 @@
 package TreesAndBinaryTrees;
 
 public class TreeCreation {
+
+    public static TreeNode create(int[] array, int index) {
+
+        if (index>= array.length) {
+            return null;
+        }
+
+        TreeNode node = new TreeNode(array[index]);
+        node.left = create(array, 2*index+1);
+        node.right = create(array, 2*index+2);
+        return node;
+    }
+
+    public static void main(String[] args) {
+        int[] input = new int[] {1,2,3,4,5,6,7};
+
+        TreeNode root = create(input, 0);
+    }
 }
